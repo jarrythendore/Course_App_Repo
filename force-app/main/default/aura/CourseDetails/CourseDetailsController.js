e163a7d9c7a1a37f5d81076c4bc47b5c4378e4c0
Ray <jarrythendore@gmail.com>
2021-09-17 11:18:10 -0500

    Update CourseDetailsController.js

--- force-app/main/default/aura/CourseDetails/CourseDetailsController.js
+++ force-app/main/default/aura/CourseDetails/CourseDetailsController.js
@@ -4,7 +4,7 @@
 		var courseIdMsg = event.getParam("courseId");
 		component.set("v.course",courseIdMsg);
 		component.set("v.id",courseIdMsg.Id);
-        //component.set("v.course.Description__c",courseIdMsg.Description__c);
+        	//component.set("v.course.Description__c",courseIdMsg.Description__c);
 		//component.find("service").reloadRecord();
 	}
 })
